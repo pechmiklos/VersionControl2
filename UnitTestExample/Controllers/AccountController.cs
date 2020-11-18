c4bcb63dc10c356c1828788c4b47eb566178415b
Miklós Péch <miklos.pech@stud.uni-corvinus.hu>
2020-11-18 20:17:13 +0100

    commit4

--- UnitTestExample/Controllers/AccountController.cs
+++ UnitTestExample/Controllers/AccountController.cs
@@ -25,13 +25,14 @@ namespace UnitTestExample.Controllers
             if(!ValidateEmail(email))
                 throw new ValidationException(
                     "A megadott e-mail cím nem megfelelő!");
-            if(!ValidateEmail(email))
+            if(!ValidatePassword(password))
                 throw new ValidationException(
                     "A megadottt jelszó nem megfelelő!\n" +
                     "A jelszó legalább 8 karakter hosszú kell legyen, csak az angol ABC betűiből és számokból állhat, és tartalmaznia kell legalább egy kisbetűt, egy nagybetűt és egy számot.");
 
             var account = new Account()
             {
+                ID = Guid.NewGuid(),
                 Email = email,
                 Password = password
             };
@@ -51,15 +52,15 @@ namespace UnitTestExample.Controllers
         public bool ValidatePassword(string password)
         {
 
-            if (!Regex.IsMatch(password, @"^[A-Za-z1-9]{8,}$"))
+           if (!Regex.IsMatch(password, @"^[A-Za-z1-9]{8,}$"))
+               return false;
+           if (!Regex.IsMatch(password, @"[a-z]"))
                 return false;
-            if (!Regex.IsMatch(password, @"[a-z]"))
+           if (!Regex.IsMatch(password, @"[A-Z]"))
                 return false;
-            if (!Regex.IsMatch(password, @"[A-Z]"))
-                return false;
-            if (! Regex.IsMatch(password, @"[1-9]"))
+           if (!Regex.IsMatch(password, @"[1-9]"))
                     return false;
-            return true;
+           return true;
             
             
 
